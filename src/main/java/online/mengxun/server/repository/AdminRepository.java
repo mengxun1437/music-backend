f99262ea3941d4459c4014f491b847e131374e66
mengxun1437 <mengxun1437@gmail.com>
2021-03-09 13:58:22 +0800

    nodejs backend

--- src/main/java/online/mengxun/server/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package online.mengxun.server.repository;
-
-import online.mengxun.server.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, String> {
-
-}
